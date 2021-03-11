d177258102a83dd4268c10f6f978d0f88dda99c3
Anand_Gogoi <anandgogoi2@gmail.com>
2021-03-11 12:32:43 -0800

    testing new move method. This will be simpler and can be used on all pieces in any direction

--- app/src/main/java/com/example/myapplication/TestingGround.java
+++ app/src/main/java/com/example/myapplication/TestingGround.java
@@ -2,8 +2,6 @@ package com.example.myapplication;
 
 public class TestingGround {
     public static void main(String[] args){
-        GameState testState = new GameState();
-        Piece testPiece = new Piece(5,5);
-        System.out.println(testState.inRange(1,1));
+
     }
 }
